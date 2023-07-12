70eb646d82c01efbfcac90dc1d7486968dbe3757
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2023-07-12 15:18:52 +0200

    added a setup file

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# taken from https://github.com/CEA-COSMIC/ModOpt/blob/master/setup.py
+with open('requirements.txt') as open_file:
+    install_requires = open_file.read()
+
+setuptools.setup(
+    name="deq-flow",
+    version="0.0.1",
+    author="Paul Micaelli",
+    author_email="paul.micaelli@ed.ac.uk",
+    description="Landmark DEQ.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/polo5/LDEQ_Rwr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+    python_requires='>=3.6',
+)
